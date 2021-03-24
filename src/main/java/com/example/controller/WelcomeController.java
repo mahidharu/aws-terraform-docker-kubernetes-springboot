3b2ee3e4a4446a89202ab58f6b7998c61d18fbf1
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-03-23 21:23:19 -0500

    Remove

--- src/main/java/com/example/controller/WelcomeController.java
+++
@@ -1,9 +0,0 @@
-@RestController
-public class WelcomeController {
-
-	@RequestMapping("/")
-	public String home(@RequestParam(value = "name", defaultValue = "home") String name) {
-		return "Home";
-
-	}
-}
