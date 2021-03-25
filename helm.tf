aa3ea9664207abd56c586e26fdfdc1851c160e67
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-03-25 01:02:04 -0500

    update

--- helm.tf
+++ helm.tf
@@ -1,17 +1,7 @@
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.default.token
+    config_path = "~/.kube/config"
   }
-}
-
-data "aws_eks_cluster" "default" {
-  name = aws_eks_cluster.demo.name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = aws_eks_cluster.demo.name
 }
 
 resource "helm_release" "nginx_ingress" {
