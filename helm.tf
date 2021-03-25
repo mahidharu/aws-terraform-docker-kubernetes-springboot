c9ed99406453efa55db46f5e75227466683b87aa
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-03-25 00:51:19 -0500

    Update

--- helm.tf
+++ helm.tf
@@ -3,7 +3,6 @@ provider "helm" {
     host                   = data.aws_eks_cluster.default.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.default.token
-    load_config_file       = false
   }
 }
 
