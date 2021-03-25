2956310bc2b2802c7da25c1ceaa9f3fd49d22f6e
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-03-25 03:12:46 -0500

    update vars

--- variables.tf
+++ variables.tf
@@ -20,13 +20,3 @@ variable "service-name" {
   default = "rest-service"
   type    = string
 }
-
-variable "aws_account_id" {
-  default     = "${TF_VAR_AWS_ACCOUNT_ID}"
-  type    = string
-}
-
-variable "repo_name" {
-  default     = "${TF_VAR_REPO_NAME}"
-  type    = string
-}
