78f9207a1a2a6afb646f124dae84a3cce068a721
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-03-28 01:42:26 -0500

    Update

--- main.tf
+++ main.tf
@@ -8,13 +8,3 @@ terraform {
     }
   }
 }
-
-//module "ec2" {
-//  source = "./modules/ec2"
-//}
-
-# Not required: currently used in conjunction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
-provider "http" {}
--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -2,3 +2,13 @@ provider "aws" {
   profile = var.env
   region  = var.aws_region
 }
+
+//module "ec2" {
+//  source = "./modules/ec2"
+//}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
