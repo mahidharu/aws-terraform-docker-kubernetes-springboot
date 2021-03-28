5be8bcd51e82480d8d979282dd80b298ad2768f1
Mahidhar <43684464+mahidharu@users.noreply.github.com>
2021-03-28 01:37:20 -0500

    Update

---
+++ main.tf
@@ -0,0 +1,20 @@
+# terraform main configuration
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+//module "ec2" {
+//  source = "./modules/ec2"
+//}
+
+# Not required: currently used in conjunction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
